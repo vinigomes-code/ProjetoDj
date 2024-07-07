cbda94acd4eb8b5d7fd7b1368e1b3b1033eaf800
Paulo Vinicius <v4mwssw@gmail.com>
2024-07-07 09:08:24 -0300

    get TDD

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
+from django.http.response import Http404
 
 from .models import Recipe
 
@@ -31,4 +32,9 @@ def recipe(request, id):
     })
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
     return render(request, 'recipes/pages/search.html')
